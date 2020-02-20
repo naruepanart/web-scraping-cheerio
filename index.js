365d7e62dd49a3b20d7165fa846fef927995b14f
Naruepanart Siangsanan <33704769+naruepanart@users.noreply.github.com>
2020-02-20 11:58:24 +0700

    youtube count

--- index.js
+++ index.js
@@ -1,20 +1,37 @@
 const express = require("express");
-const fs = require("fs");
-const csv = require("neat-csv");
+const cheerio = require("cheerio");
+const request = require("request-promise");
 const cron = require("node-cron");
-const raw = fs.readFileSync("./post.csv", "utf8");
-const FetchDATA = require("./hackernews");
-
 const app = express();
 
-const readCSV = async () => {
-  const result = await csv(raw, { headers: false });
-  app.get("/", function(req, res) {
-    res.json(result);
-  });
+const checkSubYoutubeChannel = async youtubeChannel => {
+  let res = await request(youtubeChannel);
+
+  let $ = cheerio.load(res);
+
+  let nameyoutubeChannel = $(
+    '[class="spf-link branded-page-header-title-link yt-uix-sessionlink"]'
+  ).attr("title");
+
+  let subCount = $(
+    '[class="yt-subscription-button-subscriber-count-branded-horizontal subscribed yt-uix-tooltip"]'
+  ).attr("aria-label");
+
+  return { nameyoutubeChannel, subCount };
 };
 
-readCSV();
-FetchDATA(`https://news.ycombinator.com/`);
+(async () => {
+  let youtubeChannel = "https://www.youtube.com/user/PewDiePie";
 
-app.listen(3000);
+  cron.schedule("* * * * * *", async () => {
+    const result = await checkSubYoutubeChannel(youtubeChannel);
+
+    app.get("/", (req, res) => {
+      res.json(result);
+    });
+  });
+})();
+
+app.listen(3000, () => {
+  console.log("Application is running on port 3000");
+});
